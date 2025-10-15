dfee0ad8b918d6a8b1f72ebd62a34da0017c78ff
Lorik Gagica <lorik.lg8@gmail.com>
2025-10-15 21:15:49 +0200

    Update

--- day2_sample.py
+++ day2_sample.py
@@ -1,7 +1,25 @@
 # Probability Distributions in Machine Learning 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm, binom, poisson, uniform
 
+# Gaussian Distribution
+x = np.linspace(-4, 4, 100)
+plt.plot(x, norm.pdf(x, loc=0, scale=1), label="Gaussian (u=0, s=1)")
 
+# Binomial Distribution
+n, p = 10, 0.5
+x = np.arange(0, n+1)
+plt.bar(x, binom.pmf(x, n, p), alpha=0.7, label="Binomial (n=10, p=0.5)")
 
+# Poisson Distribution
+lam = 3
+x = np.arange(0,10)
+plt.bar(x, poisson.pmf(x, lam), alpha=0.7, label="Poisson (l = 3)")
+
+plt.title("Probability Distributions")
+plt.legend()
+plt.show()
 
 
 
